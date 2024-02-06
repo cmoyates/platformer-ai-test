1fe4186e78a8f5492a75aa424a4990a8fc99e9b1
cmoyates <cmoyates@gmail.com>
2024-02-06 17:54:53 -0330

    Added level loading and text level

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+mod level;
+mod utils;
+
 use ::bevy::prelude::*;
 use bevy::window::PresentMode;
+use level::{generate_level_polygons, Polygon};
 
 fn main() {
     App::new()
@@ -13,9 +17,40 @@ fn main() {
             }),
             ..default()
         }))
+        .add_systems(Startup, s_init)
+        .add_systems(Update, s_render)
         .run();
 }
 
+#[derive(Resource)]
+pub struct Level {
+    pub polygons: Vec<Polygon>,
+    pub grid_size: f32,
+    pub size: Vec2,
+    pub half_size: Vec2,
+}
+
 pub fn s_init(mut commands: Commands) {
+    let grid_size = 32.0;
+
+    let (level_polygons, size, half_size) = generate_level_polygons(grid_size);
+
+    commands.insert_resource(Level {
+        polygons: level_polygons,
+        grid_size,
+        size,
+        half_size,
+    });
+
     commands.spawn(Camera2dBundle::default());
+}
+
+pub fn s_render(mut gizmos: Gizmos, level: Res<Level>) {
+    // Draw the level polygons
+    for polygon in &level.polygons {
+        gizmos.linestrip_2d(
+            polygon.points.iter().cloned().collect::<Vec<Vec2>>(),
+            polygon.color,
+        );
+    }
 }
